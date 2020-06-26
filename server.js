5ae7e39771dab4c5eb75c986c424b0801c96314c
Arihant416 <arihantjain416@gmail.com>
2020-06-26 15:28:14 +0530

    Add Database config

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express'),
-    app = express(),
-    PORT = process.env.PORT || 5000;
+	app = express(),
+	PORT = process.env.PORT || 5000;
 
+const connectDB = require('./config/db');
+//Connecting to Atlas
+connectDB();
 app.get('/', (req, res) => {
-    res.send('Hi');
+	res.send('Hi');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+	console.log(`Server running on port ${PORT}`);
 });
