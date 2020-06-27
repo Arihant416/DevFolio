c22f2b9b63667d56f5a20c273316478527fb89c3
Arihant416 <arihantjain416@gmail.com>
2020-06-27 11:05:38 +0530

    JWT Token Enabled

--- routes/apis/users.js
+++ routes/apis/users.js
@@ -3,6 +3,8 @@ const express = require('express'),
 const { check, validationResult } = require('express-validator')
 const gravatar = require('gravatar')
 const bcrypt = require('bcryptjs')
+const config = require('config')
+const jwt = require('jsonwebtoken')
 const User = require('../../models/User')
 const normalize = require('normalize-url')
 
@@ -57,9 +59,20 @@ router.post(
       user.password = await bcrypt.hash(password, salt)
 
       await user.save()
-
-      //Return JsonWebToken
-      res.send('User Registered')
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err
+          res.json({ token })
+        }
+      )
     } catch (error) {
       console.log(error.message)
       res.status(500).send('Server Error')
