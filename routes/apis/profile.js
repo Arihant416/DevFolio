04f4e15553f63cbcb419aa974428bc1c8c3f6e99
Arihant416 <arihantjain416@gmail.com>
2020-06-28 11:05:21 +0530

    User's profile and all profile

--- routes/apis/profile.js
+++ routes/apis/profile.js
@@ -99,4 +99,41 @@ router.post(
   }
 );
 
+// @route  GET api/profiles
+// @desc   Get all profiles
+// @access Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', [
+      'name',
+      'email',
+      'avatar'
+    ]);
+    res.json(profiles);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route Get api/profile/user/:user_id
+// @desc  Get profile by user_id
+// access Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate('user', ['name', 'email', 'avatar']);
+    if (!profile)
+      return res.status(400).json({ message: 'No such profile exists' });
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind == 'ObjectId') {
+      return res.status(400).json({ message: 'Profile not Found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
