208ef05efd9a7c6f1b67a42b07c5e5166c6b9ccc
Arihant416 <arihantjain416@gmail.com>
2020-06-30 11:29:31 +0530

    Added Post route

--- routes/apis/posts.js
+++ routes/apis/posts.js
@@ -1,11 +1,37 @@
 const express = require('express'),
 	router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../controller/authController');
+const Post = require('../../models/postModel'),
+	Profile = require('../../models/Profile'),
+	User = require('../../models/User');
+// @Route POST api/posts
+// @desc  Create a post
+// @access Private
+router.post(
+	'/',
+	[auth, [check('text', 'Text cannot be empty').not().isEmpty()]],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		try {
+			const user = await User.findById(req.user.id).select('-password');
 
-// @Route Get api/posts
-// @desc  Test User
-// @access Pulbic
-router.get('/', (req, res) => {
-	res.send('Posts Route');
-});
+			const newPost = new Post({
+				text: req.body.text,
+				name: user.name,
+				avatar: user.avatar,
+				user: req.user.id
+			});
+			const post = await newPost.save();
+			res.json(post);
+		} catch (error) {
+			console.error(error.message);
+			res.status(500).send('Server Error');
+		}
+	}
+);
 
 module.exports = router;
