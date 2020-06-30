aa9c90f11702c170d6d87308f7ee077035220e6f
Arihant416 <arihantjain416@gmail.com>
2020-06-30 11:47:54 +0530

    Delete a post By id

--- routes/apis/posts.js
+++ routes/apis/posts.js
@@ -63,4 +63,24 @@ router.get('/:id', auth, async (req, res) => {
 		res.status(500).send('Server Error');
 	}
 });
+// @route   DELETE api/posts/:id
+// @desc    Delete a post
+// @access  Private
+router.delete('/:id', auth, async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+		//Check User
+		if (post.user.toString() !== req.user.id) {
+			return res.status(401).json({ message: 'Unauthorized User' });
+		}
+		await post.remove();
+		res.json({ message: 'Post successfully removed' });
+	} catch (error) {
+		console.error(error.message);
+		if (error.kind === 'ObjectId') {
+			return res.status(404).json({ message: 'Post not found' });
+		}
+		res.status(500).send('Server Error');
+	}
+});
 module.exports = router;
