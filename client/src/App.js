6c75d76b6ed2611db930f3654005850c21aa1584
Arihant416 <arihantjain416@gmail.com>
2020-07-01 10:37:09 +0530

    Store and Reducer init

--- client/src/App.js
+++ client/src/App.js
@@ -4,21 +4,26 @@ import Navbar from './components/layouts/Navbar';
 import Landing from './components/layouts/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+//Redux Portion
+import { Provider } from 'react-redux';
+import store from './store';
 import './App.css';
 
 const App = () => (
-	<Router>
-		<Fragment>
-			<Navbar />
-			<Switch>
-				<Route exact path='/' component={Landing} />
-				<section className='container'>
-					<Route exact path='/register' component={Register} />
-					<Route exact path='/login' component={Login} />
-				</section>
-			</Switch>
-		</Fragment>
-	</Router>
+	<Provider store={store}>
+		<Router>
+			<Fragment>
+				<Navbar />
+				<Switch>
+					<Route exact path='/' component={Landing} />
+					<section className='container'>
+						<Route exact path='/register' component={Register} />
+						<Route exact path='/login' component={Login} />
+					</section>
+				</Switch>
+			</Fragment>
+		</Router>
+	</Provider>
 );
 
 export default App;
