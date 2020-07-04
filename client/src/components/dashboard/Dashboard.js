11a9f05fcccd53a46bcd25977924d30e3de44e7f
Arihant416 <arihantjain416@gmail.com>
2020-07-04 21:19:27 +0530

    Profile Component Work init

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,12 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+// import Spinner from '../layouts/Spinner';
 import { getCurrentProfile } from '../../actions/profile';
-const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+import Spinner from '../layouts/Spinner';
+const Dashboard = ({
+	getCurrentProfile,
+	auth: { user },
+	profile: { profile, loading },
+}) => {
 	useEffect(() => {
 		getCurrentProfile();
 	}, []);
-	return <div>Dashboard</div>;
+	return loading && profile === null ? (
+		<Spinner />
+	) : (
+		<Fragment>
+			<h1 className='large text-primary'>Dashboard</h1>
+			<p className='lead'>
+				<i className='fa fa-user'></i> Welcome {user && user.name}
+			</p>
+		</Fragment>
+	);
 };
 
 Dashboard.propTypes = {
---
+++ client/src/components/layouts/Spinner.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+import spinner from './spinner.gif';
+export default () => (
+	<>
+		<img
+			src={spinner}
+			style={{ margin: 'auto', display: 'block', width: '100px' }}
+			alt='Loading...'
+		/>
+	</>
+);
