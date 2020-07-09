c4ae7af46648ce457f2ecd26f2fbca38243d8d56
Arihant416 <arihantjain416@gmail.com>
2020-07-09 17:50:09 +0530

    Education Added

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import DashboardActions from './DashboardActions';
 import Experience from './Experience';
+import Education from './Education';
 // import Spinner from '../layouts/Spinner';
 import { getCurrentProfile } from '../../actions/profile';
 import Spinner from '../layouts/Spinner';
@@ -26,6 +27,7 @@ const Dashboard = ({
 			{profile !== null ? (
 				<Fragment>
 					<Experience experience={profile.experience} />
+					<Education education={profile.education} />
 				</Fragment>
 			) : (
 				<Fragment>
