b85baa1eb93b2b1dda1ed7e957d82d0a04efa9d4
Arihant416 <arihantjain416@gmail.com>
2020-07-15 14:55:24 +0530

    Profile Started

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -43,10 +43,10 @@ export const getProfiles = () => async dispatch => {
 	}
 };
 //get profile by id
-export const getProfilebyId = userId => async dispatch => {
-	// dispatch({ type: CLEAR_PROFILE })0;
+export const getProfileById = userId => async dispatch => {
 	try {
-		const res = await axios.get(`api/profile/user/${userId}`);
+		const res = await axios.get(`/api/profile/user/${userId}`);
+
 		dispatch({
 			type: GET_PROFILE,
 			payload: res.data,
