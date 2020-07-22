7be221e09f3a9c1bb303e49f07d8ce73536e3df4
Arihant416 <arihantjain416@gmail.com>
2020-07-22 20:42:54 +0530

    Add Delete & update Posts

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR } from './types';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from './types';
 
 //Get Posts
 export const getPosts = () => async dispatch => {
@@ -17,6 +17,60 @@ export const getPosts = () => async dispatch => {
 				msg: err.response.statusText,
 				status: err.response.status,
 			},
+		});
+	}
+};
+//Add A Like
+export const addLike = id => async dispatch => {
+	try {
+		const res = await axios.put(`/api/posts/like/${id}`);
+		dispatch({
+			type: UPDATE_LIKES,
+			payload: { id, likes: res.data },
+		});
+	} catch (err) {
+		dispatch({
+			type: POST_ERROR,
+			payload: {
+				msg: err.response.statusText,
+				status: err.response.status,
+			},
+		});
+	}
+};
+
+//Remove A Like
+export const removeLike = id => async dispatch => {
+	try {
+		const res = await axios.put(`/api/posts/unlike/${id}`);
+		dispatch({
+			type: UPDATE_LIKES,
+			payload: { id, likes: res.data },
+		});
+	} catch (err) {
+		dispatch({
+			type: POST_ERROR,
+			payload: {
+				msg: err.response.statusText,
+				status: err.response.status,
+			},
+		});
+	}
+};
+
+//Delete Post
+export const deletePost = id => async dispatch => {
+	try {
+		const res = await axios.delete(`/api/posts/${id}`);
+		dispatch({
+			type: DELETE_POST,
+			payload: id,
+		});
+		dispatch(setAlert('Post Removed Successfully', 'success'));
+	} catch (err) {
+		dispatch({
+			type: POST_ERROR,
+			payload: { msg: err.response.statusText, status: err.response.status },
 		});
 	}
 };
--- client/src/components/posts/Post.js
+++ client/src/components/posts/Post.js
@@ -3,11 +3,27 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getPosts } from '../../actions/post';
 import Spinner from '../layouts/Spinner';
+import PostItem from './PostItem';
 const Post = ({ getPosts, post: { posts, loading } }) => {
 	useEffect(() => {
 		getPosts();
 	}, [getPosts]);
-	return <div></div>;
+	return loading ? (
+		<Spinner />
+	) : (
+		<Fragment>
+			<h1 className='large text-primary'>Posts</h1>
+			<p className='lead'>
+				<i className='fa fa-user'></i> Welcome to the Community
+			</p>
+			{/* {Post Forum} */}
+			<div className='posts'>
+				{posts.map(post => (
+					<PostItem key={post._id} post={post} />
+				))}
+			</div>
+		</Fragment>
+	);
 };
 
 Post.propTypes = {
